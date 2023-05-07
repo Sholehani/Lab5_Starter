01c2c519caadf883c4782a29bc8e9c4fae43bb22
Sholehani Hafezi <10mariashol15@gmail.com>
2023-05-07 07:08:56 -0700

    finished explore pt 1 and 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lab 5 - Starter
 ## Names: Sholehani Hafezi
+
+## Expose Pt. 2 link: https://github.com/Sholehani/intro-github-actions.git
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,68 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+let synth = window.speechSynthesis;
+
 function init() {
-  // TODO
+  let exploreSec = document.querySelector("#explore");
+
+  const smileImg = exploreSec.querySelector("img");
+  const talkButton = exploreSec.querySelector("button");
+
+  const voiceSelect = exploreSec.querySelector("#voice-select");
+  const inputTxt = exploreSec.querySelector("#text-to-speak");
+
+  console.log(voiceSelect.value);
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  inputTxt.onsubmit = (event) => {
+    event.preventDefault();
+  }
+
+  talkButton.addEventListener("click", (event) => {
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+
+    smileImg.src = "assets/images/smiling-open.png";
+
+    utterThis.addEventListener("end", function () {
+      console.log(speechSynthesis.speaking);
+      smileImg.src = "assets/images/smiling.png";
+    });
+    inputTxt.blur();
+  });
+
+
+
+
 }
